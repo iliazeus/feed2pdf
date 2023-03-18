0059b1b4579cacc700760170915e280b864b8d16
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2023-03-18 23:19:25 +0600

    add release action

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ esbuild \
   --bundle --sourcemap=inline --minify --charset=utf8 \
   --platform=node --banner:js="#!/usr/bin/env node" \
   ./src/cli.ts \
-  --outfile=./dist/cli.js
+  --outfile=./dist/feed2pdf.js
